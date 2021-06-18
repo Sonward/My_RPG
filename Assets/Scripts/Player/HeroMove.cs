ed0c55b2f5c5fd52f5770ba2e0271a53e88ec25f
Sonward <poliseroma@gmail.com>
2021-06-19 01:53:18 +0300

    Update HeroMove.cs

--- Assets/Scripts/Player/HeroMove.cs
+++ Assets/Scripts/Player/HeroMove.cs
@@ -7,8 +7,6 @@ public class HeroMove : MonoBehaviour
     [SerializeField] private float cameraSize = 10f;
     [SerializeField] private float speed = 15f;
 
-    //private bool walk;
-
     private Rigidbody2D rb;
     private Vector3 mousePosition3D;
     private Vector2 movement;
@@ -21,7 +19,6 @@ public class HeroMove : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
         Camera.main.orthographicSize = cameraSize;
-        //walk = false;
     }
 
     // Update is called once per frame
@@ -32,17 +29,6 @@ public class HeroMove : MonoBehaviour
 
         movement.x = Input.GetAxis("Horizontal");
         movement.y = Input.GetAxis("Vertical");
-
-        //if (Input.GetMouseButtonDown(1))
-        //{
-        //    walk = true;
-        //}
-
-        //if (walk)
-        //{
-        //    transform.position = Vector3.MoveTowards(transform.position, mousePosition3D, speed * Time.deltaTime);
-        //}
-        //if (transform.position == mousePosition3D) { walk = false; }
     }
     private void FixedUpdate()
     {
