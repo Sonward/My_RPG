985f3cb66d41219fcfeeaf58ee91b8cb16bbec34
Sonward <poliseroma@gmail.com>
2021-06-19 01:51:51 +0300

    a5

--- Assets/Scripts/NPCs/NPCsScript.cs
+++ Assets/Scripts/NPCs/NPCsScript.cs
@@ -26,6 +26,14 @@ public class NPCsScript : MonoBehaviour
     {
         healthPoints -= damage;
         Debug.Log("NPC get damage: " + damage);
-        if (healthPoints<=0) { isDead = true; Debug.Log("NPC was murdered"); }
+        if (healthPoints<=0) { Death(); Debug.Log("NPC was murdered"); }
+    }
+
+    private void Death()
+    {
+        isDead = true;
+        GetComponent<PolygonCollider2D>().enabled = false;
+        transform.GetChild(2).gameObject.SetActive(true);
+        transform.GetChild(0).gameObject.SetActive(false);
     }
 }
--- Assets/Scripts/Player/HeroMove.cs
+++ Assets/Scripts/Player/HeroMove.cs
@@ -7,9 +7,11 @@ public class HeroMove : MonoBehaviour
     [SerializeField] private float cameraSize = 10f;
     [SerializeField] private float speed = 15f;
 
-    private bool walk;
+    //private bool walk;
 
+    private Rigidbody2D rb;
     private Vector3 mousePosition3D;
+    private Vector2 movement;
 
     public float Speed { get => speed; }
     public Vector3 MousePosition3D { get => mousePosition3D; }
@@ -17,9 +19,9 @@ public class HeroMove : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //speed = 15f;
+        rb = GetComponent<Rigidbody2D>();
         Camera.main.orthographicSize = cameraSize;
-        walk = false;
+        //walk = false;
     }
 
     // Update is called once per frame
@@ -28,15 +30,22 @@ public class HeroMove : MonoBehaviour
         mousePosition3D = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         mousePosition3D.z = transform.position.z;
 
-        if (Input.GetMouseButtonDown(1))
-        {
-            walk = true;
-        }
+        movement.x = Input.GetAxis("Horizontal");
+        movement.y = Input.GetAxis("Vertical");
 
-        if (walk)
-        {
-            transform.position = Vector3.MoveTowards(transform.position, mousePosition3D, speed * Time.deltaTime);
-        }
-        if (transform.position == mousePosition3D) { walk = false; }
+        //if (Input.GetMouseButtonDown(1))
+        //{
+        //    walk = true;
+        //}
+
+        //if (walk)
+        //{
+        //    transform.position = Vector3.MoveTowards(transform.position, mousePosition3D, speed * Time.deltaTime);
+        //}
+        //if (transform.position == mousePosition3D) { walk = false; }
+    }
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
     }
 }
