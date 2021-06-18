4509fc59bd43853f4d1d09de1cd8cdd7329d9213
Sonward <poliseroma@gmail.com>
2021-06-19 01:15:19 +0300

    a2

---
+++ Assets/Scripts/NPCs/NPCsScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCsScript : MonoBehaviour
+{
+    [SerializeField] private int healthPoints = 10;
+
+    public int Healthpoints { get => healthPoints; set => healthPoints = value; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
