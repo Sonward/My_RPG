367a0287929d6506de64852a5ef854ff6fcdc62c
Sonward <poliseroma@gmail.com>
2021-06-26 01:09:31 +0300

    NPCs AI works

--- Assets/Scripts/NPCs/NPCsAI.cs
+++ Assets/Scripts/NPCs/NPCsAI.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class NPCsAI : MonoBehaviour
 {
     private GameObject player;
+    private bool fightMode = false;
+    enum FightStates { dontFight, fight }
+    FightStates previousState;
+    FightStates currentState;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentState =  FightStates.dontFight;
     }
 
     // Update is called once per frame
@@ -24,9 +28,33 @@ public class NPCsAI : MonoBehaviour
         {
             player = null;
         }
+
+        if (player != null) { previousState = currentState; currentState = FightStates.fight; }
+        else { previousState = currentState; currentState = FightStates.dontFight; }
+
+        if (previousState != currentState) { ChangeFightMode(); }
+
         if (Input.GetKeyDown(KeyCode.E))
         {
             Debug.Log(player);
+        }
+    }
+
+    private void ChangeFightMode()
+    {
+        if (currentState == FightStates.fight)
+        {
+            transform.GetChild(4).gameObject.SetActive(true);
+            transform.GetChild(3).gameObject.SetActive(true);
+            transform.GetChild(0).gameObject.SetActive(false);
+            fightMode = true;
+        }
+        else 
+        {
+            transform.GetChild(4).gameObject.SetActive(false);
+            transform.GetChild(3).gameObject.SetActive(false);
+            transform.GetChild(0).gameObject.SetActive(true);
+            fightMode = false; 
         }
     }
 }
